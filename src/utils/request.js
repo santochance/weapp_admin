4e348e1572dc34f81db93900b8fb439e722a7e09
Santochance <santochance@qq.com>
2018-02-04 12:01:41 +0800

    服务端请求路径添加'/api/v1'

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import { notification } from 'antd';
 import { routerRedux } from 'dva/router';
 import store from '../index';
 
-const HOST = 'https://vc-weapp.leanapp.cn';
+const HOST = 'https://vc-weapp.leanapp.cn/api/v1';
 
 const codeMessage = {
   200: '服务器成功返回请求的数据',
