6549d4f8ba3d6338dd7bcc6e813a7b86d4e715eb
Vincent <santochance@qq.com>
2018-02-05 12:24:44 +0800

    修改Ueditor后端请求接口路径

--- public/vendor/ueditor/ueditor.config.js
+++ public/vendor/ueditor/ueditor.config.js
@@ -20,7 +20,7 @@
          * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
          */
         var URL = window.UEDITOR_HOME_URL || getUEBasePath();
-
+        var HOST = 'https://vc-weapp.leanapp.cn/api/v1';
         /**
          * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
          */
@@ -30,7 +30,7 @@
             UEDITOR_HOME_URL: URL
 
             // 服务器统一请求接口路径
-            , serverUrl: 'https://vc-weapp.leanapp.cn/ueditor'
+            , serverUrl: HOST + '/ueditor'
 
             //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
             , toolbars: [[
