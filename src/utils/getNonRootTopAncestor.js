89aede5f975e90dae0ff3328ffa601ab9c1bd93f
Santochance <santochance@qq.com>
2018-02-16 08:31:10 +0800

    添加findTreeNode()和getNonRootTopAncestor()

---
+++ src/utils/getNonRootTopAncestor.js
@@ -0,0 +1,39 @@
+
+function findTreeNode(_list, iteratee) {
+  function recur(list) {
+    let target = null;
+    for (let i = 0; i < list.length; i += 1) {
+      // 如果自己满足iteratee, 返回这个元素
+      const item = list[i];
+      if (iteratee(item, i, list)) {
+        target = item;
+        break;
+      }
+      // 否则检查是否有children
+      if (item.children && item.children.length > 0) {
+        target = recur(item.children);
+        // 如果有满足iteratee的后代, 返回这个后代元素
+        if (target) {
+          break;
+        }
+      }
+    }
+    // 如果到这里target还为空，说明当前list及后代没有满足iteratee的元素
+    // 返回null
+    return target;
+  }
+
+  return recur(_list);
+}
+
+function getNonRootTopAncestor(flatList, _node) {
+  // 往上遍历直到找到父节点是根节点的祖先节点
+  function recur(node) {
+    const { pid } = node;
+    if (!pid) return node;
+    return recur(flatList[pid]);
+  }
+  return recur(_node);
+}
+
+module.exports = { findTreeNode, getNonRootTopAncestor };
